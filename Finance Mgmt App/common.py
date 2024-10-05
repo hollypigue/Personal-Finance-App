ce03bdd11a6b00f76078d7d12a01af806a04b098
Holly <112795891+hollypigue@users.noreply.github.com>
2024-10-05 12:02:03 -0500

    Add files via upload

---
+++ Finance Mgmt App/common.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.graphics import Color, Rectangle, RoundedRectangle
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.dropdown import DropDown
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.togglebutton import ToggleButton
+from kivy.core.window import Window
